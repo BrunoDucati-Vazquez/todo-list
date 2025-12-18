8eff0c77cbeb1a9a4365259599289fbb1359c497
BrunoDucati-Vazquez <bducativazquez@gmail.com>
2025-12-18 17:25:15 -0300

    feat: Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Text from "./components/text";
 
 export default function App() {
  
   return (
-  <h1 className="text-pink-dark">Hello world!  </h1>
+    <div className="flex flex-col gap- 2">
+      <Text as="p" variant="body-sm-bold" className="text-pink-dark">Hello world!</Text>
+      <Text as="p" className="text-green-dark">Hello world!</Text>
+      <Text as="p" variant="body-md-bold">Hello world!</Text>
+    </div>
+    
   )
 }
 
