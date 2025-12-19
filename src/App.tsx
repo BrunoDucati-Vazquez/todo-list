822ea9cc680eff048503878cb8aedc134e32b572
BrunoDucati-Vazquez <bducativazquez@gmail.com>
2025-12-18 21:38:50 -0300

    feat: Icon component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/icon";
+
 
 export default function App() {
  
   return (
-    <div className="flex flex-col gap- 2">
-      <Text as="p" variant="body-sm-bold" className="text-pink-dark">Hello world!</Text>
-      <Text as="p" className="text-green-dark">Hello world!</Text>
-      <Text as="p" variant="body-md-bold">Hello world!</Text>
-    </div>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap- 2">
+        <Text as="p" variant="body-sm-bold" className="text-pink-dark">Hello world!</Text>
+        <Text as="p" className="text-green-dark">Hello world!</Text>
+        <Text as="p" variant="body-md-bold">Hello world!</Text>
+      </div>
     
+
+    <div className="flex gap-1">
+      <Icon svg={TrashIcon} />
+      <Icon svg={CheckIcon} />
+      <Icon svg={PencilIcon} />
+      <Icon svg={PlusIcon} />
+      <Icon svg={SpinnerIcon} animate={true} />
+      <Icon svg={XIcon} />
+    </div>
+  </div>
   )
 }
-
-App;
