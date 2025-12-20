c41c49371c5bb4c91df1b0cb92468015b73182bf
BrunoDucati-Vazquez <bducativazquez@gmail.com>
2025-12-20 16:57:52 -0300

    feat: Card Component Creation

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
 
 export default function App() {
  
@@ -49,6 +50,9 @@ export default function App() {
     </div>
     <div>
       <InputCheckbox/>
+    </div>
+    <div>
+      <Card>Olá Mundo</Card>
     </div>
   </div>
   )
